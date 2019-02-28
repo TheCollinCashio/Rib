72e25d3b412f7a8d09029db62f497b4d713a7f85
Tyler <tylercarr1111@yahoo.com>
2019-02-28 15:35:33 -0600

    Changed how express static works and move starting server to setPort

--- src/Rib.ts
+++ src/Rib.ts
@@ -3,17 +3,15 @@ const express = require('express')
 const app = express()
 let port: Number
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
 export function setPort(portNumber: Number) {
     port = portNumber
+    app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 }
 
 export function setDefaultRoute(request: String, filename: String) {
-    console.log(filename)
     app.get(request, (req, res) => res.sendFile(filename))
 }
 
 export function setClientFolder(folderName: String) {
-    app.use(folderName, express.static(folderName))
+    app.use(express.static(folderName))
 }
