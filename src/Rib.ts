bf09686dd4711167461e97d305121556d435fd98
Tyler <tylercarr1111@yahoo.com>
2019-02-28 15:57:56 -0600

    Added socket io and created functions

--- src/Rib.ts
+++ src/Rib.ts
@@ -1,17 +1,27 @@
 const express = require('express')
-
 const app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+
+io.on('connection', function(socket) {
+    console.log('a user connected')
+})
+
 let port: Number
 
 export function setPort(portNumber: Number) {
     port = portNumber
-    app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+    http.listen(port, () => console.log(`Example app listening on port ${port}!`))
 }
 
-export function setDefaultRoute(request: String, filename: String) {
-    app.get(request, (req, res) => res.sendFile(filename))
+export function setDefaultRoute(request: String, fileName: String) {
+    app.get(request, (req, res) => res.sendFile(fileName))
 }
 
 export function setClientFolder(folderName: String) {
     app.use(express.static(folderName))
 }
+
+export function createRoute(routeName: String) {}
+
+export function createRoutewithResult() {}
