2b1c4f3dcfc88f5f3128c374399b550402071c23
Collin Cashio <collin.cashio@selu.edu>
2019-02-28 14:02:40 -0600

    working

--- src/Rib.ts
+++ src/Rib.ts
@@ -1,5 +1,15 @@
-console.log('test');
-let x = 1
+import express from 'express'
 
+const app = express()
+const port = 3000
 
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+export function createDefaultRoute(request, filename) {
+    app.get(request, (req, res) => res.sendFile(__dirname + '/' + filename))
+}
+
+export function serveClientFolder(folderName) {
+    app.use('/client', express.static(__dirname + '/client'))
+}
 
