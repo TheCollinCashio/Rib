d4318e8f737c6147c7b1c44539762f0ea2426c62
Collin Cashio <collin.cashio@selu.edu>
2019-03-01 22:09:37 -0600

    cleaned up test file

--- test/server.js
+++ test/server.js
@@ -2,6 +2,11 @@ import Rib from '../lib/server/Rib'
 Rib.startServer(5000, 'This is much easier to program')
 
 let myRib = new Rib()
+myRib.onConnect(() => {
+    setInterval(() => {
+        myRib.tellAllClientsHello('Hello from the other side')
+    }, 2000)
+})
 
 myRib.setDefaultRoute('/', `${__dirname}/client/index.html`)
 myRib.setClientFolder([`${__dirname}/client/build`])
@@ -15,9 +20,3 @@ function sayLove() {
 }
 
 myRib.exposeFunctions([sayHello, sayLove])
-
-myRib.onConnect(() => {
-    setInterval(() => {
-        myRib.tellAllClientsHello('Hello from the other side')
-    }, 2000)
-})
