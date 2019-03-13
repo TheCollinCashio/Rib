3754604d17c4995e5f60c9a791a42af6c30a40ef
Collin Cashio <collin.cashio@selu.edu>
2019-03-13 10:44:44 -0500

    turn this repo into a example app

---
+++ client/src/stores/appStore.js
@@ -0,0 +1,10 @@
+import { ClientStore } from 'rib-store'
+
+let appStore = new ClientStore({
+    name: null,
+    theme: {
+        color: 'blue'
+    }
+})
+
+export default appStore
--- test/starter.js
+++
@@ -1,6 +0,0 @@
-//  Transpile All Code with Babel and use 'env' (ES6+) Preset
-require('@babel/register')()
-require('@babel/polyfill')
-
-// Import Application
-module.exports = require('./server.js')
